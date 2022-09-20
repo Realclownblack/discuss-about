6e1e3a74ff4fcf3b2070342129c84cf5292f4cda
realclownblack <danilowilliam3255@gmail.com>
2022-09-20 16:46:07 -0300

    20/09/2022 - 16:58

--- src/components/items/Box_post.jsx
+++ src/components/items/Box_post.jsx
@@ -1,8 +1,16 @@
 import '../../assets/css/css-items/box_items.css'
 
-export const Box_post = () => {
+export const Box_post = (props) => {
     return (
         <div className="Box">
+            <div className='text-box'>
+                <div className='category'>{props.category}</div>
+                <div className='title'>{props.title}</div>              
+            </div>
+            <div className='contect'>{props.contect}</div>
+            <div className='barra-opcoes'>
+                <button className='comment-button'>Comment</button>
+            </div> 
         </div>
     )
 }
--- src/components/posts/Posts.jsx
+++ src/components/posts/Posts.jsx
@@ -2,38 +2,28 @@ import { Box_post } from '../../components/items/Box_post'
 import { Barra_post } from '../../components/items/Barra_post'
 import { Notificantion_post } from '../items/Notificantion_post'
 import '../../assets/css/posts.css'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+
 
 export const Posts = () => {
+    const [posts, setPosts] = useState([]);
+
+    const fetchPosts = async () => {
+        const { data } = await axios.get('http://localhost:3000/post')
+        setPosts(data)
+        console.log(data)
+    }
+
+    useEffect(() => {
+        fetchPosts()
+    }, [])
+
     return (
         <div className="Posts">
             <div className='Posts-intems'>
                 <div className='feed-post'>
-                    <Box_post/>
-                    <div className='fack-div'></div>
-                    <Box_post/>
-                    <div className='fack-div'></div>
-                    <Box_post/>
-                    <div className='fack-div'></div>
-                    <Box_post/>
-                    <div className='fack-div'></div>
-                    <Box_post/>
-                    <div className='fack-div'></div>
-                    <Box_post/>
-                    <div className='fack-div'></div>
-                    <Box_post/>
-                    <div className='fack-div'></div>
-                    <Box_post/>
-                    <Box_post/>
-                    <div className='fack-div'></div>
-                    <Box_post/>
-                    <div className='fack-div'></div>
-                    <Box_post/>
-                    <div className='fack-div'></div>
-                    <Box_post/>
-                    <div className='fack-div'></div>
-                    <Box_post/>
-                    <div className='fack-div'></div>
-                    <Box_post/>
+                    {posts.map((post) => <Box_post category={post.category} title={post.title} contect={post.content} />)}
                     <div className='fack-div'></div>
                     <Barra_post/>
                 </div>
