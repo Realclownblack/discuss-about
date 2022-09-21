5dc2f2ac8e020c14ae37df8ecf2376bf84cfb043
realclownblack <danilowillim3255@gmail.com>
2022-09-21 15:06:36 -0300

    21/09/2022 - 15:06

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,8 +1,11 @@
 import '../../assets/css/login.css'
+import '../../assets/css/stars.css'
 import React, { useEffect} from "react";
 import {useNavigate } from "react-router-dom";
 import { auth, signInWithGoogle } from "../../firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
+import google from '../../assets/img/google.png'
+import logo from '../../assets/img/logo.png'
 
 export const Login = () => {
     const [user, loading] = useAuthState(auth);
@@ -15,11 +18,17 @@ export const Login = () => {
         if (user) navigate("/home");
       }, [user, loading]);
     return (
-    <div className="login">
-        <button className="login__btn login__google" onClick={signInWithGoogle}>
-          Login with Google
-        </button>
-        
-    </div>
+      <div className='Login-div'>
+        <div className='div-stars'>
+          <div class="stars" ></div>
+        </div>
+        <div className="Login">
+          <img className='logo_2' src={logo} alt="" />
+            <button className="login__btn login__google" onClick={signInWithGoogle}>
+              <img className='img-google' src={google} alt="" /> Login with Google
+            </button>
+        </div>       
+      </div>
+
     )
 }
