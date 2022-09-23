608775dfce4ec9c5ae38a6f2229bcbfb6d13d9e3
realclownblack <danilowilliam3255@gmail.com>
2022-09-23 11:30:43 -0300

    23/09/2022 - 11:30

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -29,8 +29,8 @@ export const Menu = () => {
             <ul class="menu">
                 <li title="home"><a href="#" onClick={() => setMenuVisible()} class="menu-button home">menu</a></li>
                 <li title="search"><a href="#" class="search">search</a></li>
-                <li title="pencil"><a href="#" class="pencil">pencil</a></li>
-                <li title="about"><a href="#" class="active about">about</a></li>
+                <li title="Criar"><a href="#" class="pencil">Criar</a></li>
+                <li title="Perfil"><a href="#" class="active about">about</a></li>
                 <li title="archive"><a href="#" class="archive">archive</a></li>
                 <li title="Logout"><a href="#" class="fa fa-sign-out" aria-hidden="true"  onClick={() => logout()} >Logout</a></li>
             </ul>
@@ -38,9 +38,10 @@ export const Menu = () => {
             <ul class={"menu-bar " + visible}>
                 <li><a href="#" onClick={() => setMenuVisible()}>Menu</a></li>
                 <li><a href="#">Home</a></li>
-                <li><a href="#">Profile</a></li>
-                <li><a href="#">Editorial</a></li>
+                <li><a href="#">Perfil</a></li>
+                <li><a href="#">Criar</a></li>
                 <li><a href="#">About</a></li>
+                <li><a href="#" onClick={() => logout()} >Logout</a></li>
             </ul>
         </div>
 
