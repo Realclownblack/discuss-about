07d30c28195391dd0b6155f4e41f5d69397783c9
Danilo William de Souza Elias <46456154839>
2022-10-19 09:33:59 -0300

    19/10/2022 - 09:24

--- src/components/items/Box_post.jsx
+++ src/components/items/Box_post.jsx
@@ -5,15 +5,16 @@ import { Commenter } from '../../components/items/Commenter'
 import one_pice from '../../assets/img/onepice.jfif'
 import { Commenter_post } from './commenter_post'
 
-export const Box_post = (props) => {
+
+export const Box_post = (props) => {    
     return (
         <div className="Box">
             <div className='info-box'>
                 <div className='info-perfil'>
-                    <div className='foto-perfil'><img className='foto-img' src={foto} alt="" /></div>
-                    <div>
-                        <div>Danilo william</div>
-                        <div>16:57 - 1 Set.</div>
+                    <div className='foto-perfil'><img className='foto-img' src={props.foto} alt="" /></div>
+                    <div className='info-name-date'>
+                        <div>{props.name}</div>
+                        <div>{props.date}</div>
                     </div>                     
                 </div>
                 <div>
@@ -21,9 +22,7 @@ export const Box_post = (props) => {
                 </div>
             </div>
             <div className='contect'>{props.contect}
-            <div className='text-post'>
-            Os capítulos têm sido serializados na revista Weekly Shōnen Jump desde 22 de julho de 1997,
-            com os capítulos compilados e publicados em 103 volumes tankōbon pela editora Shueisha até agosto de 2022. One Piece conta as aventuras de Monkey D.</div>
+            <div className='text-post'>{props.discritivo}</div>
             <div className='img-post'><img className='img-postada' src={one_pice} alt="" /></div>
             </div>
             <hr className='hr-box' />
@@ -32,32 +31,6 @@ export const Box_post = (props) => {
                 <div className='commenter-box-1'>
                     <Commenter_post commenter_users={'amo one pice amo luffy otimo personagem'}/>
                     <div className='fack-div'></div>
-                    <Commenter_post commenter_users={'acho q mae do luffy seja onisama'}/>
-                    <div className='fack-div'></div>
-                    <Commenter_post commenter_users={'para todo mundo sabe q ele filhp dp croc'}/>
-                    <div className='fack-div'></div>
-                    <Commenter_post commenter_users={'para de chapar louco'}/>
-                    <div className='fack-div'></div>
-                    <Commenter_post commenter_users={'e se no fim onepice seja uma metafora kkk'}/>
-                    <div className='fack-div'></div>
-                    <Commenter_post commenter_users={'vai tomar seus remedios'}/>
-                    <div className='fack-div'></div>
-                    <Commenter_post commenter_users={'kkkkkkkkk so noia prefiro naruto'}/>
-                    <div className='fack-div'></div>
-                    <Commenter_post commenter_users={'kkkkkkkkk so noia prefiro naruto'}/>
-                    <div className='fack-div'></div>
-                    <Commenter_post commenter_users={'kkkkkkkkk so noia prefiro naruto'}/>
-                    <div className='fack-div'></div>
-                    <Commenter_post commenter_users={'kkkkkkkkk so noia prefiro naruto'}/>
-                    <div className='fack-div'></div>
-                    <Commenter_post commenter_users={'kkkkkkkkk so noia prefiro naruto'}/>
-                    <div className='fack-div'></div>
-                    <Commenter_post commenter_users={"kkkkkkkkk so noia prefiro naruto rftdggfgtdvcryvcvcvcvcyvycvycvycvychvcvcvcvcvcvcvcvcvcvcvcvcvcvcvchvcgggggggggg ,ffhhhhhjjljkjljlpkpkkhjjhjkhgkgjh ,Os capítulos têm sido serializados na revista Weekly Shōnen Jump desde 22 de julho de 1997,com os capítulos compilados e publicados em 103 volumes tankōbon pela editora dcfxrtsusssssssssssssssssssssssssssssssssssfvcdgxtxstttttttttststststststststststststststststststststststststststststststststststststststststststststststststststststststststststststststststststShueisha até agosto de 2022. One Piece conta as aventuras de Monkey D.tfrdgdgdgvtfgrtfghtgfhtgfhvgfhy"}/>
-                    <div className='fack-div'></div>
-                    <Commenter_post commenter_users={"kkkkkkkkk so noia prefiro naruto rftdggggggggggggffhhhhhjjljkjljlpkpkkhjjhjkhgkgjh Os capítulos têm sido serializados na revista Weekly Shōnen Jump desde 22 de julho de 1997,com os capítulos compilados e publicados em 103 volumes tankōbon pela editora Shueisha até agosto de 2022. One Piece conta as aventuras de Monkey D.tfrdgdgdgvtfgrtfghtgfhtgfhvgfhy"}/>
-                    <div className='fack-div'></div>
-                    <Commenter_post commenter_users={'kkkkkkkkk so noia prefiro naruto rftdggggggggggggffhhhhhjjljkjljlpkpkkhjjhjkhgkgjh Os capítulos têm sido serializados na revista Weekly Shōnen Jump desde 22 de julho de 1997,com os capítulos compilados e publicados em 103 volumes tankōbon pela editora Shueisha até agosto de 2022. One Piece conta as aventuras de Monkey D.tfrdgdgdgvtfgrtfghtgfhtgfhvgfhy'}/>
-                    <Commenter_post commenter_users={"kk pelomenos os files de one pice e bom kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk Os capítulos têm sido serializados na revista Weekly Shōnen Jump desde 22 de julho de 1997,com os capítulos compilados e publicados em 103 volumes tankōbon pela editora Shueisha até agosto de 2022. One Piece conta as aventuras de Monkey D."}/>
                 </div>
                 <div className='commenter-box-2'>
                     <div className='foto-perfil-user'><img className='foto-img' src={foto} alt="" /></div>
--- src/components/posts/Posts.jsx
+++ src/components/posts/Posts.jsx
@@ -2,21 +2,52 @@ import { Box_post } from '../../components/items/Box_post'
 import { Create_ilha } from '../create/Create_ilha';
 import { Menu } from '../menu/Menu';
 import '../../assets/css/posts.css'
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import axios from 'axios'
+import {collection, query, orderBy, onSnapshot} from "firebase/firestore"
+import {db} from '../../firebase'
 
 
 export const Posts = () => {
     const [posts, setPosts] = useState([]);
+    const fetchei = useRef(false);
 
     const fetchPosts = async () => {
         const { data } = await axios.get('http://localhost:3000/post')
         setPosts(data)
         console.log(data)
     }
-  
+
+    const fecthFirebase = async() => {
+        const temp = []
+        onSnapshot(query(collection(db, 'posts')), (querySnapshot) => {
+            querySnapshot.docs.map((doc) => {
+                temp.push({
+                    data: doc.data()['data'],
+                    descritivo: doc.data()['descritivo'],
+                    titulo: doc.data()['titulo'],
+                    foto: doc.data()['foto']
+                })
+            })
+        })
+        setPosts(temp)
+    }
+
+
     useEffect(() => {
-        fetchPosts()
+        onSnapshot(query(collection(db, 'posts')), (querySnapshot) => {
+            const temp = []
+            querySnapshot.docs.map((doc) => {
+                temp.push({
+                    data: doc.data()['data'],
+                    descritivo: doc.data()['descritivo'],
+                    titulo: doc.data()['titulo'],
+                    foto: doc.data()['foto'],
+                    nome: doc.data()['name']
+                })
+            })
+            setPosts(temp)
+        })
     }, [])
 
     return (
@@ -25,15 +56,17 @@ export const Posts = () => {
                 <div className='feed-post'>
                     <Create_ilha/>
                     <div className='fack-div'></div>
-                    <Box_post />
-                    <div className='fack-div'></div>
-                    <Box_post />
-                    <div className='fack-div'></div>
-                    <Box_post />
-                    <div className='fack-div'></div>
-                    <Box_post />
-                    <div className='fack-div'></div>
-                    {/* {posts.map((post) => <Box_post category={post.category} title={post.title} contect={post.content} />)} */}
+                    <br /><br />
+                    {
+                        posts.length > 0
+                        ?
+                        posts.map((item) => 
+                           <Box_post discritivo={item.descritivo} date={item.data} name={item.nome} foto={item.foto}/>
+                        )
+                        :
+                        <p>vc corno</p>
+                    }   
+                    <div className='fack-div</div>'></div>       
                 </div>
             </div>
             <Menu/>
